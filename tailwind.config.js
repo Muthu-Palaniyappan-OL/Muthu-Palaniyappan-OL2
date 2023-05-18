3ffd7a2be5ff4fbec2307b566f3066ab56723509
Muthu Palaniyappan OL <muthu892542@gmail.com>
2023-05-18 06:48:59 +0530

    Base

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      'blue': '#007aff',
+      'green': '#2fd159',
+      'dark-blue': '#5e5be6',
+      'orange': '#ff9f0a',
+      'pink': '#ff2c55',
+      'purple': '#af52de',
+      'red': '#ff3c44',
+      'sky-blue': '#63d2ff',
+      'yellow': '#63d2ff',
+      'white': '#ffffff',
+      'black': '#000000'
+    },
+  },
+  plugins: [],
+}
+
